8e0bd96189b12c1860578349d5c2c8d2a77d53b6
Gelo <angelo.delossantos000@gmail.com>
2024-05-12 22:09:36 +0800

    Login and Register pages
    cleaned and adjusted according to design with slight changes.

--- DeskIt-Hot-Desk-Booking-System/resources/views/auth/forgot-password.blade.php
+++ DeskIt-Hot-Desk-Booking-System/resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,34 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    <div class="sm:rounded-lg px-6 py-3 bg-yellowA">
+        {{-- Heading --}}
+        <div class="flex justify-start py-3">
+            <h1 class="text-2xl font-semibold">Forgot your password?</h1>
+        </div>
+        <div class="mb-4 text-md text-gray-900 font-light">
+            {{ __('No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        </div>
+
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+
+            <!-- Email Address -->
+            <div>
+                <x-input-label for="email" :value="__('Email')" />
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+            </div>
+
+            <!-- Session Status -->
+            <x-auth-session-status class="mb-4 text-yellowB" :status="session('status')" />
+
+            <div class="flex items-center justify-end mt-4">
+                <x-primary-button>
+                    {{ __('Email Password Reset Link') }}
+                </x-primary-button>
+            </div>
+        </form>
+
+
     </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
 </x-guest-layout>
--- DeskIt-Hot-Desk-Booking-System/resources/views/auth/reset-password.blade.php
+++ DeskIt-Hot-Desk-Booking-System/resources/views/auth/reset-password.blade.php
@@ -1,4 +1,5 @@
 <x-guest-layout>
+
     <form method="POST" action="{{ route('password.store') }}">
         @csrf
 
@@ -8,7 +9,7 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
+            <x-text-input id="email" class="block mt-1 w-full text-gray-500" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" disabled/>
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
@@ -36,4 +37,5 @@
             </x-primary-button>
         </div>
     </form>
+
 </x-guest-layout>
