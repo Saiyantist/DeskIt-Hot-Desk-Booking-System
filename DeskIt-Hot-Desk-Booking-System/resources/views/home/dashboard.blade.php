ce72ba8e3af915adf55a14e359211f69c43fda92
nnajah1 <jannahdelarosa65@gmail.com>
2024-05-23 16:04:59 +0800

    User Dashboard Modification
    - edited home controller
    - edited css bg color
    - added icon images
    - edited user dashboard page

--- DeskIt-Hot-Desk-Booking-System/resources/views/home/dashboard.blade.php
+++ DeskIt-Hot-Desk-Booking-System/resources/views/home/dashboard.blade.php
@@ -1,21 +1,65 @@
 @extends('layouts.layout')
 <x-app-layout>
     @section('content')
-    <div class="head-uHome mt-16">
-        <img src="../images/head-uHome.png" class="img-fluid">
-        <h1>Welcome! Let's find the right workspace for you.</h1>
-        <a class="book" wire:navigate href="{{ route('book') }}">Book a desk</a>
-    </div>
-    <section>
-        <div class="ml-44 mt-16">
-            <div class="myBook-container">
-                <div class="my-bookings">
-                    <h5 class="text-white">My bookings</h5>
+    <section class="bg-pink">
+        <div class="mt-16" >
+            <h2 class="pl-32 pt-16">Good Morning,
+                <span class=" text-yellowB"> {{auth::user()->name}}</span>
+            </h2>
+            <h6 class="pl-32 font-light text-lg">Let's do the best today!</h6>
+        </div>
+        <div>
+            
+        </div>
+        <div class="ml-24 mt-4">
+            <div class="myBook-container flex flex-row justify-evenly">
+                <div>
+                    <h6 class=" font-semibold">TODAY'S BOOKING</h6>
+                    <div class=" bg-white p-3 rounded-lg drop-shadow-lg mt-3">
+                        <h6 class="flex"><img src="{{ asset('images/calendar.svg') }}" class="pr-2"> insert date here</h6>
+                        <h6 class="flex"><img src="{{ asset('images/location.svg') }}"class="pr-2">insert floor & desk here</h6>
+                        <h6 class="flex"><img src="{{ asset('images/clock.svg') }}"class="pr-2">insert time here</h6>
+                    </div>  
                 </div>
-                <div class="flex w-3/4 h-screen bg-grey p-4 absolute">
+                <div class="w-3/4 h-[650px] ">
                     <div id="calendar"></div>
                 </div>
+                
             </div>
+        </div>
+    </section>
+
+    <section class="bg-pink flex justify-center items-center"> 
+        <div class=" ml-24 p-2 flex flex-col bg-white rounded-lg w-[90%] drop-shadow-lg"> 
+            <div class="flex justify-between p-2">
+                <h6>Upcoming Bookings</h6>
+                <h6>Bookings <i class="fa-solid fa-arrow-right"></i></h6>
+            </div>
+            <table class=" text-center">
+                <thead class="bg-gray">
+                    <tr>
+                        <th class="w-1/12">ID</th>
+                        <th class="w-1/12">Name</th>
+                        <th class="w-1/12">Email</th>
+                        <th class="w-1/12">Floor #</th>
+                        <th class="w-1/12">Desk #</th>
+                        <th class="w-1/12">Date</th>
+                        <th class="w-1/12">Booked By</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {{-- @foreach($bookingsData as $booking)
+                    <tr>
+                        <td>{{ $booking['Id'] }}</td>
+                        <td>{{ $booking['Name'] }}</td>
+                        <td>{{ $booking['Date'] }}</td>
+                        <td>{{ $booking['Desk ID'] }}</td>
+                        <td>{{ $booking['Status'] }}</td>
+                    </tr>
+                    @endforeach --}}
+                </tbody>
+            </table>
+
         </div>
     </section>
     @push('scripts')
@@ -36,6 +80,28 @@
         td.fc-daygrid-day {
             height: 11.9vh;
         }
+        
+        div.fc-event-main {
+        background-color: #F9F6F6;
+        color: black;
+        border: none;
+        border-left: 5px solid #FBB503;
+        padding: 3px;
+       }
+       div.fc-daygrid {
+        background-color: white;
+       }
+
+       div#calendar {
+        background-color: white;
+        height: 15vh;
+        border-radius: 10px;
+       }
+
+       div.fc-header-toolbar {
+        padding: 15px 15px 0 15px;
+       }
+
     </style>
     <script>
         document.addEventListener('DOMContentLoaded', function () {
@@ -52,7 +118,8 @@
                     start: startDate,
                     end: endDate
                 },
-                events: "{{ route('user.bookings', auth()->id()) }}",
+                
+                events: " <img src={{ asset('images/location.svg') }}> {{ route('user.bookings', auth()->id()) }}",
                 eventClick: function (info) {
                     // Handle event click if needed
                 }
