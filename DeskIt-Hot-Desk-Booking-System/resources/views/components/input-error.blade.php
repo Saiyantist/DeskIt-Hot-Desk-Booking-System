d4b6ee33ebb051364687f8110248a0529d94e428
joyiee2314 <113758261+joyiee2314@users.noreply.github.com>
2024-06-04 16:48:23 +0800

    input-error update

--- DeskIt-Hot-Desk-Booking-System/resources/views/components/input-error.blade.php
+++ DeskIt-Hot-Desk-Booking-System/resources/views/components/input-error.blade.php
@@ -1,16 +1,7 @@
-<!-- resources/views/components/input-error.blade.php -->
-@if ($messages)
+@props(['messages'])
 
-    <div class="flex-initial justify-end drop-shadow-md pt-3 text-center ">
-        <div class="alert alert-warning border border-2 ml-6 w-69  ">
-            <div class="flex justify-center">
-                <img src="{{ asset('images/!.svg') }}"class="h-4 w-4 " alt="Password requirements icon">
-            </div>
-            <p class="text-xs">
-                @foreach ($messages as $message)
-                    {{ $message }}
-                @endforeach
-            </p>
-        </div>
-    </div>
-@endif
+<ul {{ $attributes->merge(['class' => 'text-sm text-red-600 dark:text-red-400 space-y-1']) }}>
+    @foreach ((array) $messages as $message)
+        <li>{{ $message }}</li>
+    @endforeach
+</ul>
