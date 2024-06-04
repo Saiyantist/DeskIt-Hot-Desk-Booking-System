cb2062bc66152c2444d0304e16e98e674ec54806
joyiee2314 <113758261+joyiee2314@users.noreply.github.com>
2024-06-04 19:18:41 +0800

    design error message

--- DeskIt-Hot-Desk-Booking-System/resources/views/profile/partials/update-password-form.blade.php
+++ DeskIt-Hot-Desk-Booking-System/resources/views/profile/partials/update-password-form.blade.php
@@ -24,14 +24,21 @@
         <div class="row">
             <div class="col-md-12 flex">
                 <div class="flex-1">
-                    <label for="current_password">Current Password</label>
-                    <input type="password" id="current_password" wire:model.defer="current_password" class="form-control">
-                </div> 
-                
-                <div class="flex-1">
-                    @error('current_password') <span class="error">{{ $message }}</span> @enderror
-                 </div>
-                
+                  <label for="current_password">Current Password</label>
+                  <input type="password" id="current_password" wire:model.defer="current_password" class="form-control">
+                </div>
+
+                <div class="flex-1 ml-3 mt-3">
+                  @error('current_password')
+                    <div class="alert alert-warning flex items-center py-2 px-4 rounded shadow-md text-sm" role="alert">
+                      <span class="mr-2">
+                        <img src="{{ asset('images/!.svg') }}" class="h-5 w-5" alt="Password requirements icon">
+                      </span>
+                      {{ $message }}
+                    </div>
+                  @enderror
+                </div>
+             
             </div>
 
             {{-- new password --}}
@@ -41,9 +48,17 @@
                     <input type="password" id="password" wire:model.defer="password" class="form-control mt-1">
                 </div>
 
-                <div class="flex-1">
-                    @error('password') <span class="error">{{ $message }}</span> @enderror
-                </div>
+                <div class="flex-1 ml-3 mt-3">
+                    @error('password')
+                      <div class="alert alert-warning flex items-center py-2 px-4 rounded shadow-md text-sm" role="alert">
+                        <span class="mr-2">
+                          <img src="{{ asset('images/!.svg') }}" class="h-6 w-6" alt="Password requirements icon">
+                        </span>
+                        {{ $message }}
+                      </div>
+                    @enderror
+                  </div>
+
             </div>
 
             {{-- confirm new password --}}
@@ -54,14 +69,22 @@
                     
                 </div>
 
-                <div class="flex-1">
-                    @error('password_confirmation') <span class="error">{{ $message }}</span> @enderror
-                </div>
+                <div class="flex-1 ml-3 mt-3">
+                    @error('password_confirmation')
+                      <div class="alert alert-warning flex items-center py-2 px-4 rounded shadow-md text-sm" role="alert">
+                        <span class="mr-2">
+                          <img src="{{ asset('images/!.svg') }}" class="h-6 w-6" alt="Password requirements icon">
+                        </span>
+                        {{ $message }}
+                      </div>
+                    @enderror
+                  </div>
+
             </div>
 
             {{-- forget password --}}
             <div class="relative justify-end">
-                <a href="" class="mt-1 text-sm text-gray-600 dark:text-gray-400">Forget password</a>
+                <a href="{{ route('password.request') }}" class="mt-1 text-sm text-gray-600 dark:text-gray-400">Forget password</a>
             </div>
         </div>
 
