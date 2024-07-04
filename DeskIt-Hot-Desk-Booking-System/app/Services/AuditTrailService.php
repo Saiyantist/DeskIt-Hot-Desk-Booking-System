a6aa7951a2adebcc046ea86bce2365c23d85b5e0
Kertsu <kurtddbigtas@gmail.com>
2024-07-04 16:27:55 +0800

    add migration

---
+++ DeskIt-Hot-Desk-Booking-System/app/Services/AuditTrailService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\AuditTrail;
+
+class AuditTrailService
+{
+    public function createTrail($email = null, $action_type, $action_details, $status, $additional_context = null)
+    {
+        AuditTrail::create([
+            'user_id' => auth()->id(),
+            'email' => $email,
+            'action_type' => $action_type,
+            'action_details' => $action_details,
+            'status' => $status,
+            'additional_context' => $additional_context,
+            'ip_address' => request()->ip()
+        ]);
+    }
+}
---
+++ DeskIt-Hot-Desk-Booking-System/database/migrations/2024_07_04_072840_create_audit_trails_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('audit_trails', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('email')->nullable();
+            $table->string('action_type');
+            $table->string('action_details');
+            $table->string('status');
+            $table->string('additional_context')->nullable();
+            $table->string('ip_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('audit_trails');
+    }
+};
